545393cef43332ac873aeae11950e1f5997a5d03
Ismail Akçelep <dewo9666@gmail.com>
2021-09-19 04:56:10 +0300

    media container being added

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <div class="container">
     <Header />
     <router-view/>
-    
+<media-container/>
   </div>
 </template>
 <script>
 import Header from "./views/header/header.vue";
 import * as request from "./axios" 
+import mediaContainer from "./views/mediaPlayerPopup/mediaContainer.vue"
 export default {
   components: {
     Header,
+    mediaContainer,
   },
   created() {
     request.getUsers().then((musicData) => {
--- src/views/mediaPlayerPopup/mediaContainer.vue
+++ src/views/mediaPlayerPopup/mediaContainer.vue
@@ -1,13 +1,37 @@
 <template>
-  <div> deneme </div>
+<div class="container" > 
+
+<div class="soungVideoPlayer"> <videoPlayer/> </div>
+
+<div class="musicPlayerPage">Bazı önerilen Muzikler Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus officia earum tenetur tempora provident, illum amet omnis nobis, recusandae nihil delectus obcaecati necessitatibus. Vel, laudantium repudiandae debitis animi officiis quibusdam?</div>
+
+
+
+
+    </div>
 </template>
 
 <script>
+import videoPlayer from "./videoPlayer.vue"
 export default {
-
+components:{
+    videoPlayer
+}
 }
 </script>
 
-<style>
+<style scoped >
+.container {
+    margin-top:3vw;
+    width: 100%;
+    height: 100%;
+    z-index: 97;
+    position: fixed;
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
 
 </style>
